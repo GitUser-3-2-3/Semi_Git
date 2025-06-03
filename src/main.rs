fcbd5884c314d4b76e74bcd5e6ccfdc3f2888930
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-06-04 02:14:45 +0530

    git hash-object feature added
    Took 2 hours 47 minutes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use anyhow::Context;
 use clap::{Parser, Subcommand};
+use flate2::Compression;
 use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use sha1::{Digest, Sha1};
 use std::ffi::CStr;
-use std::io::{BufRead, BufReader, Read};
+use std::io::{BufRead, BufReader, Read, Write};
+use std::path::{Path, PathBuf};
 use std::{fs, io};
 
 #[derive(Parser, Debug)]
@@ -20,6 +24,12 @@ enum Command {
         pretty_print: bool,
 
         object_hash: String,
+    },
+    HashObject {
+        #[clap(short = 'w')]
+        write: bool,
+
+        file: PathBuf,
     },
 }
 
@@ -98,11 +108,77 @@ fn main() -> anyhow::Result<()> {
                 }
             }
         }
+        Command::HashObject { write, file } => {
+            fn write_blob<W>(file: &Path, writer: W) -> anyhow::Result<String>
+            where
+                W: Write,
+            {
+                let stat =
+                    fs::metadata(&file).with_context(|| format!("stat {}", file.display()))?;
+
+                let writer = ZlibEncoder::new(writer, Compression::default());
+
+                let mut writer = HashWriter {
+                    writer,
+                    hasher: Sha1::new(),
+                };
+
+                write!(writer, "blob ")?;
+                write!(writer, "{}\0", stat.len())?;
+
+                let mut file =
+                    fs::File::open(&file).with_context(|| format!("open {}", file.display()))?;
+                io::copy(&mut file, &mut writer).context("stream file into blob")?;
+
+                let _ = writer.writer.finish()?;
+                let hash = writer.hasher.finalize();
+                Ok(hex::encode(hash))
+            }
+
+            let hash = if write {
+                let tmp = "temporary";
+                let hash = write_blob(
+                    &file,
+                    fs::File::create(tmp).context("construct temporary file for blob")?,
+                )
+                .context("write out blob object")?;
+
+                fs::create_dir_all(format!(".git/objects/{}/", &hash[..2]))
+                    .context("create subdir of .git/objects")?;
+
+                fs::rename(tmp, format!(".git/objects/{}/{}", &hash[..2], &hash[2..]))
+                    .context("move blob files into .git/objects")?;
+                hash
+            } else {
+                write_blob(&file, io::sink()).context("write out blob object")?
+            };
+            println!("{hash}")
+        }
     }
     Ok(())
 }
 
-// one way to limit the size of a reader
+struct HashWriter<W> {
+    writer: W,
+    hasher: Sha1,
+}
+
+impl<W> Write for HashWriter<W>
+where
+    W: Write,
+{
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let size = self.writer.write(buf)?;
+        self.hasher.update(&buf[..size]);
+        Ok(size)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        todo!()
+    }
+}
+
+// One way to limit the size of a reader. Alternative on line 92
 
 /*
 struct LimitReader<R> {
